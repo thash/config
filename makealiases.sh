231d9186cb052bddca822f70ba12b30c6ee297c1
takuyahashimoto <takuya21hashimoto@gmail.com>
2011-01-24 21:42:32 +0900

    mod makealiases script

--- makealiases.sh
+++ makealiases.sh
@@ -4,8 +4,10 @@ for filename in vimrc gvimrc bashrc zshrc zshenv screenrc gitconfig
 do
     if [ -e $HOME/.$filename ]; then 
         rm -i $HOME/.$filename 
+        ln -s `pwd`/.$filename $HOME/.$filename
+    else
+        ln -s `pwd`/.$filename $HOME/.$filename
     fi
-    ln -s `pwd`/.$filename $HOME/.$filename
 done
 
 source ~/.zshrc
