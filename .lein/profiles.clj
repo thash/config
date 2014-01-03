f293c9a6722de82232c9d7f36d8cb063dc3b00bd
memerelics <takuya21hashimoto@gmail.com>
2014-01-03 22:55:43 +0900

    use lein-droid as a leiningen plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,6 @@
-{:user  {:plugins  []
+{:user  {:plugins  [
+                    [lein-droid "0.2.0-preview4"] ;; latest 0.2.0 causes instant killing.
+                    ]
+         :android {:sdk-path "/Users/hash/work/android/sdk/"}
          :search-page-size 30
          :jvm-opts ["-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1"]}}
