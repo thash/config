fbba7268672d91faf6a73e10fd66b38fff04e3ea
memerelics <takuya21hashimoto@gmail.com>
2014-01-15 01:32:37 +0900

    use lein-localrepo, update lein-droid to latest 0.2.0, and setup android keystore-path

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,9 @@
 {:user  {:plugins  [
-                    [lein-droid "0.2.0-preview4"] ;; latest 0.2.0 causes instant killing.
+                    [lein-droid "0.2.0"]
+                    [lein-localrepo "0.5.3"]
                     ]
-         :android {:sdk-path "/Users/hash/work/android/sdk/"}
+         :android {:sdk-path "/Users/hash/work/android/sdk/"
+                   ;; :keystore-path "/Users/hash/.android/memerelics.keystore"
+                   }
          :search-page-size 30
          :jvm-opts ["-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1"]}}
