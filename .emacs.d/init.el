4ea7e7720d7e64b1b37a1a51bd1bad8d9b51e55e
memerelics <takuya21hashimoto@gmail.com>
2014-08-31 22:11:47 +0900

    [Emacs] tweak settings of OCaml(tuareg-mode)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -486,13 +486,13 @@
 ;; OCaml (tuareg-mode) (elpa)
 (autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
 (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
-; (autoload 'tuareg-imenu-set-imenu "tuareg-imenu"
-;   "Configuration of imenu for tuareg" t)
-; (add-hook 'tuareg-mode-hook 'tuareg-imenu-set-imenu)
+;; (autoload 'tuareg-imenu-set-imenu "tuareg-imenu"
+;;   "Configuration of imenu for tuareg" t)
+;; (add-hook 'tuareg-mode-hook 'tuareg-imenu-set-imenu)
 (setq auto-mode-alist
-  (append '(("\\.ml[ily]?$" . tuareg-mode)
-     ("\\.topml$" . tuareg-mode))
-     auto-mode-alist))
+      (append '(("\\.ml[ily]?$" . tuareg-mode)
+                ("\\.topml$" . tuareg-mode))
+              auto-mode-alist))
 
 ;; Oz(Mozart2)
 (defun run-oz-env ()
