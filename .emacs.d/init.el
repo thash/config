14ca407a13c84d394bec29d73aed688c242eccb4
memerelics <takuya21hashimoto@gmail.com>
2015-06-30 08:32:29 +0900

    [emacs] marmalde repo 2

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 ;; package.el
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("marmalade2" . "http://marmalade-repo.org/"))
 (package-initialize)
 
 ;; load-path
