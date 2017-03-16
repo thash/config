6653aa16832ff7a70857f6561419faf1d3af07e7
thash <hash@hzl.jp>
2017-03-16 22:34:23 +0900

    [lein] ultra

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,11 @@
 {:user  {:plugins  [
-                    [lein-droid "0.2.0"]
+                    ;; [lein-droid "0.2.0"]
                     [lein-localrepo "0.5.3"]
+                    [venantius/ultra "0.5.1"]
+                    [nightlight/lein-nightlight "1.6.1"]
                     ]
-         :android {:sdk-path "/Users/hash/work/android/sdk/"
-                   ;; :keystore-path "/Users/hash/.android/memerelics.keystore"
-                   }
+         ;; :android {:sdk-path "/Users/hash/work/android/sdk/"
+         ;;           ;; :keystore-path "/Users/hash/.android/memerelics.keystore"
+         ;;           }
          :search-page-size 30
          :jvm-opts ["-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1"]}}
